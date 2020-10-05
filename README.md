120e244e4f59a7562f4fea121bbbcea3c99930e3
Austen Rhyce Erickson <austenrerickson@Austens-MacBook-Pro-2.local>
2020-10-05 16:18:26 -0500

    finished code and updated README

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # plotly-challenge
-Plotly Homework Assignment: Belly-Button Biodiversity
+This repository contains an exercise for using Plotly in Javascript.  
+
+The exercise utilizes data collect by an experiment that examines the biodiversity of bacteria found in belly-buttons.  
+
+Further details on the nature of this exercise and the data utilized can be found in the Instructions Folder.  
+
+Note: Much of the code written was developed with in-class guidance from my instructor.  
--- static/js/app.js
+++ static/js/app.js
@@ -1,41 +1,39 @@
-console.log("I have belly-button");
-
-//* <select id="selDataset" onchange="optionChanged(this.value)"></select>
-
-//<div id="sample-metadata" class="panel-body"></div>
-
-//* <div id="bar"></div> */
-
-//<div id="gauge"></div>
-
-//<div id="bubble"></div> */
+//Console log something
+console.log("Using app.js") ;
 
 
-
+//Function to fill panel with metadata
 function FillMetaData(sampleID)
+
     {
+        //Log something to confirm function has run
         console.log(`FillMetaData(${sampleID})`) ;
 
+        //Get all data
         d3.json("data/samples.json").then((data) =>
 
         {
-
+        //Get all metadata
         var metadata = data.metadata ;
     
-
         function selectSampleMeta(ID)
+
             {
                 return ID.id == sampleID ;
             }
-
+        
+        //Get metadata for sampleID
         var sampleMetaArray = metadata.filter(selectSampleMeta) ;
 
         var metaResult = sampleMetaArray[0] ;
-
+        
+        //Link to panel in HTML
         var panel = d3.select("#sample-metadata") ;
-
+        
+        //Clear existing metadata
         panel.html("") ;
         
+        //Fill panel with sample metadata
         Object.entries(metaResult).forEach(([key, value]) => 
 
             {
@@ -45,53 +43,31 @@ function FillMetaData(sampleID)
             }) ;
 
 
-
-
-        // console.log(sampleMetaArray) ;
-
-        // console.log(sampleMetaArray[0]) ;
+        }) ;
         
-        // sampleMetaArray.forEach((metadatum) =>
-        //     {
-
-        //         console.log(metadatum) ;
-
-        //         Object.entries(metadatum).forEach(([key, value]) =>
-        //             {
-
-                        
-        //                 // meta.text(sampleMeta[0]).property("value", sampleMeta[0]) ;
-                        
-        //                 console.log(`key: ${key} and value: ${value}`) ;
-
-        //                 // var meta = d3.select(".panel-body") ;
-        //                 var meta = d3.select("#sample-metadata") ;
-
-        //                 meta.text(key, value).property(key, value) ;
-                
-        //             }) ;
-
-        //     });
-
-    
-    
-        });
     } ;
 
+
+//Function to draw bar graph
 function DrawBarGraph(sampleID)
+
     {
+        //Log something to confirm function has run
         console.log(`DrawBarGraph(${sampleID})`) ;
-        // var bar = d3.select("#bar") ;
 
+        //Get all data
         d3.json("data/samples.json").then((data) =>
-            {
 
+            {
+                //Get all sample data
                 var samples = data.samples ;
 
+                //Get data for sampleID
                 var resultArray = samples.filter(s => s.id == sampleID) ;
 
                 var result = resultArray[0] ;
 
+                //Build bar graph
                 var otu_ids = result.otu_ids ;
 
                 var otu_labels = result.otu_labels ;
@@ -117,29 +93,74 @@ function DrawBarGraph(sampleID)
                         title: "Top 10 Bacteria Cultures Found",
                         margin: {t: 30, l: 150}
                     }
-
+                
+                //Draw bar graph
                 Plotly.newPlot("bar", [barData], barLayout) ;
 
-            });
-
+            }) ;
 
     } ;
 
 
-
-
+//Functio to draw bubble chart
 function DrawBubbleChart(sampleID)
+
     {
+        //Console log something to confirm the function has been run
         console.log(`DrawBubbleChart(${sampleID})`) ;
 
-         // var bubble = d3.select("#bubble") ;
+        //Get all sample data
+         d3.json("data/samples.json").then((data) =>
+
+            {
+                //Get all sample data
+                var samples = data.samples ;
+
+                //Get sample data for sampleID
+                var resultArray = samples.filter(s => s.id == sampleID) ;
+
+                var result = resultArray[0] ;
+
+                //Build bubble chart
+                var otu_ids = result.otu_ids ;
+
+                var otu_labels = result.otu_labels ;
+
+                var sampleValues = result.sample_values ;
+
+                var plotBubble =
+
+                    {
+                        x: otu_ids,
+                        y: sampleValues ,
+                        mode: 'markers',
+                        marker: {size: sampleValues, color: otu_ids},
+                        text: otu_labels
+                    } ;
+
+                var layout =
+                    {
+                        xaxis: {title: "OTU ID"},
+                        height: 600,
+                        width: 1000
+                    };
+                
+                //Draw bubble chart
+                Plotly.newPlot("bubble", [plotBubble], layout) ;
+
+            }) ;
 
     } ;
 
+
+//Function to run on new subject ID selection
 function optionChanged(newSampleID)
-    {
+
+    {   
+        //Log new selection
         console.log(`User selected ${newSampleID}`) ;
 
+        //Run functions to display data
         FillMetaData(newSampleID) ;
 
         DrawBarGraph(newSampleID) ;
@@ -149,24 +170,24 @@ function optionChanged(newSampleID)
     } ;
 
 
-
+//Function to run to initialize the webpage dashboard
 function InitDash() 
-    {
 
+    {
+        //Console log initialization
         console.log(`Calling InitDash()`) ;
 
-        //Filling Selector
-
+        //Link to Subject ID selector
         var selector = d3.select("#selDataset") ;
 
+        //Get all data
         d3.json("data/samples.json").then((data) => 
 
             {
+                //Log all data
                 console.log(data) ;
 
-
-
-                //FILLING SELECTOR
+                //Get sample ID names
                 var sampleNames = data.names ;
 
                 //Populate the selector with all of the sample IDs
@@ -180,20 +201,21 @@ function InitDash()
 
                 }) ;
 
+                //Choose starting sample ID
                 var sampleID = sampleNames[0] ;
 
                 console.log("Starting Sample: ", sampleID) ;
 
+                //Initialize page by displaying data for starting sample ID
                 FillMetaData(sampleID) ;
                
-
                 DrawBarGraph(sampleID) ;
             
-
                 DrawBubbleChart(sampleID) ;
 
             }) ;
 
-    }
+    } ;
 
+//Initialize Dashboard
 InitDash() ;
