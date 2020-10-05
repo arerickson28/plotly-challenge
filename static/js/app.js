43f6c55985239bafb786ec557f959d7253840616
Austen Rhyce Erickson <austenrerickson@Austens-MacBook-Pro-2.local>
2020-10-04 21:44:22 -0500

    finished metadata portion

--- static/js/app.js
+++ static/js/app.js
@@ -21,21 +21,59 @@ function FillMetaData(sampleID)
         {
 
         var metadata = data.metadata ;
+    
 
         function selectSampleMeta(ID)
             {
                 return ID.id == sampleID ;
             }
 
-        var sampleMeta = metadata.filter(selectSampleMeta) ;
+        var sampleMetaArray = metadata.filter(selectSampleMeta) ;
+
+        var metaResult = sampleMetaArray[0] ;
+
+        var panel = d3.select("#sample-metadata") ;
+
+        panel.html("") ;
         
+        Object.entries(metaResult).forEach(([key, value]) => 
 
-        console.log(sampleMeta) ;
+            {
+                var textToShow = `${key}: ${value}`
+                panel.append("h6").text(textToShow) ;
+               
+            }) ;
 
-        var meta = d3.select(".panel-body") ;
-        meta.text(sampleMeta[0]).property("value", sampleMeta[0]) ;
 
 
+
+        // console.log(sampleMetaArray) ;
+
+        // console.log(sampleMetaArray[0]) ;
+        
+        // sampleMetaArray.forEach((metadatum) =>
+        //     {
+
+        //         console.log(metadatum) ;
+
+        //         Object.entries(metadatum).forEach(([key, value]) =>
+        //             {
+
+                        
+        //                 // meta.text(sampleMeta[0]).property("value", sampleMeta[0]) ;
+                        
+        //                 console.log(`key: ${key} and value: ${value}`) ;
+
+        //                 // var meta = d3.select(".panel-body") ;
+        //                 var meta = d3.select("#sample-metadata") ;
+
+        //                 meta.text(key, value).property(key, value) ;
+                
+        //             }) ;
+
+        //     });
+
+    
     
         });
     } ;
@@ -43,11 +81,59 @@ function FillMetaData(sampleID)
 function DrawBarGraph(sampleID)
     {
         console.log(`DrawBarGraph(${sampleID})`) ;
+        // var bar = d3.select("#bar") ;
+
+        d3.json("data/samples.json").then((data) =>
+            {
+
+                var samples = data.samples ;
+
+                var resultArray = samples.filter(s => s.id == sampleID) ;
+
+                var result = resultArray[0] ;
+
+                var otu_ids = result.otu_ids ;
+
+                var otu_labels = result.otu_labels ;
+
+                var sampleValues = result.sample_values ;
+
+                var yticks = otu_ids.slice(0, 10).map(otuId => `OTU ${otuId}`).reverse() ;
+
+                var barData =
+
+                    {
+                        x: sampleValues.slice(0, 10).reverse(),
+                        y: yticks,
+                        type: "bar",
+                        text: otu_labels.slice(0, 10).reverse(),
+                        orientation: "h"
+
+                    }
+
+                var barLayout = 
+
+                    {
+                        title: "Top 10 Bacteria Cultures Found",
+                        margin: {t: 30, l: 150}
+                    }
+
+                Plotly.newPlot("bar", [barData], barLayout) ;
+
+            });
+
+
     } ;
+
+
+
 
 function DrawBubbleChart(sampleID)
     {
         console.log(`DrawBubbleChart(${sampleID})`) ;
+
+         // var bubble = d3.select("#bubble") ;
+
     } ;
 
 function optionChanged(newSampleID)
@@ -72,9 +158,6 @@ function InitDash()
         //Filling Selector
 
         var selector = d3.select("#selDataset") ;
-        // var meta = d3.select("#sample-metadata") ;
-        // var bar = d3.select("#bar") ;
-        // var bubble = d3.select("#bubble") ;
 
         d3.json("data/samples.json").then((data) => 
 
